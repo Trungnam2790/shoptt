20ad6d98d0273f3c8515c6be589cfd566f6e23fa
Pham Trung Nam <phamtrungnam279@gmail.com>
2025-05-02 01:11:30 +0700

    tailwind-daisiui-thiet ke giao dien co ban

--- apps/shoptt-frontend/src/app/app.component.ts
+++ apps/shoptt-frontend/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { FaIconLibrary, FaConfig, FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  standalone: true,
+  imports: [RouterModule, FaIconComponent, NavbarComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'shoptt-frontend';
+export class AppComponent implements OnInit {
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome(): void {
+    this.faConfig.defaultPrefix = 'far'; // ✅ đúng chỗ
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
