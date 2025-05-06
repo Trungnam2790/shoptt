23632aeab5f838169cce93d6d6064af00cb01024
Pham Trung Nam <phamtrungnam279@gmail.com>
2025-05-06 07:30:41 +0700

    them backend va sua sever

---
+++ apps/shoptt-backend/src/main/java/fr/codecake/shoptt/order/infrastructure/secondary/repository/JpaOrderedProductRepository.java
@@ -0,0 +1,9 @@
+//package fr.codecake.shoptt.order.infrastructure.secondary.repository;
+//
+//import fr.codecake.shoptt.order.infrastructure.secondary.entity.OrderedProductEntity;
+//import fr.codecake.shoptt.order.infrastructure.secondary.entity.OrderedProductEntityPk;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface JpaOrderedProductRepository extends JpaRepository<OrderedProductEntity, OrderedProductEntityPk> {
+//
+//}
